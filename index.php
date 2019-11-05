eb972824108c9ceb46f071b30271ed967896482b
Simthem <themthem58@gmail.com>
2019-11-05 14:13:40 +0100

    final presentation [without colors] for the index form

--- index.php
+++ index.php
@@ -145,9 +145,9 @@ if($user) {
                                     </div>
                                     <label class="mt-auto mb-auto ml-4 pl-1 text-center" for="">Dont :</label>
                                 </div>
-                                <div class="col-8 d-inline-flex pr-0 pl-0 ml-auto">
-                                    <input type="time" id="night_hours" name="night_hours" class="col-6 form-control text-center align-middle m-auto p-1 " style="line-height: 25px;" placeholder="minutes/heures">
-                                    <label class="mt-auto ml-3 mb-auto text-wrap">heures de nuit</label>
+                                <div class="col-7 d-inline-flex m-auto text-center pr-0 pl-0 mt-auto mb-auto">
+                                    <input type="time" id="night_hours" name="night_hours" class="col-7 form-control text-center align-middle p-0 mt-auto mb-auto" style="line-height: 25px;" placeholder="minutes/heures">
+                                    <label class="col-6 mt-auto ml-5 mb-auto text-wrap text-left">heures de nuit</label>
                                 </div>
                             </div>
                             <div class="mt-2 mb-2 pt-5 pb-2">
