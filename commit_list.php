e2f8249a32ffa79c06c295c0a894052f989464ba
Simthem <themthem58@gmail.com>
2019-11-08 21:43:43 +0100

    first try with collapse submenu [only in the index page at now] - try to have a full screen for apple device to [cause when we change of page we come back to safari web browser not in full screen] - fix calendar error in index page later [it's just a line and i don't know why]

--- commit_list.php
+++ commit_list.php
@@ -86,9 +86,10 @@ ORDER BY
 
 <html class="overflow-y mb-0">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="S.K.elec">
         <link  rel="stylesheet" href="assets/css/bootstrap.css">
         <link rel="stylesheet" href="assets/css/style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -169,7 +170,15 @@ ORDER BY
             </div>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="js/script.js"></script>
+        <script src="/js/jQuery.stayInWebApp-master/jquery.stayInWebApp.js"></script>
+        <script src="/js/jQuery.stayInWebApp-master/jquery.stayInWebApp.min.js"></script>
         <script src="js/bootstrap.js"></script>
+        <script>
+            $(function() {
+                $.stayInWebApp();
+            });
+        </script>
     </body>
 </html>
